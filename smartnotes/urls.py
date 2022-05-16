2af086f57312d89e52003d13030dd46afe12094d
David Montanez <dmontanez@aceproject.space>
2022-05-16 17:51:56 -0500

    Initialize Home app with Templates directory for home

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Added URLS
+    path('home', views.home)
 ]
