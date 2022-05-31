5d206dfc57fcc7bc99a01a7c2bf95e28d2e6de95
David Montanez <dmontanez@aceproject.space>
2022-05-31 15:15:36 -0500

    Attached Notes model to AdminModel
    Manually attach notes to adminmodel so it can be displayed in the admin dashboard

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    pass
+
+## register that the notes model is attached to this admin model 
+admin.site.register(models.Notes, NotesAdmin)
