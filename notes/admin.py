c59e83d4bb9b220453ee5d2acb77d4812bba4f08
David Montanez <dmontanez@aceproject.space>
2022-05-31 15:17:37 -0500

    Added Table Columns
    Manually configure which fields you want to display for that particular note object on the table

--- notes/admin.py
+++ notes/admin.py
@@ -4,7 +4,7 @@ from . import models
 
 # Register your models here.
 class NotesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'text', 'created')
 
 ## register that the notes model is attached to this admin model 
 admin.site.register(models.Notes, NotesAdmin)
