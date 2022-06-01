fcbe99cf83fd25b35f2861a43dd16289952b0b67
David Montanez <dmontanez@aceproject.space>
2022-06-01 08:57:01 -0500

    Configure class-based view to Note endpoints

--- notes/views.py
+++ notes/views.py
@@ -1,18 +1,29 @@
 from doctest import DocFileSuite
+from typing import List
 from django.shortcuts import render
 
 from .models import Notes
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NoteListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = 'notes/notes_list.html'
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
         
-    return render(request, 'notes/notes_detail.html', {'note': note})
+#     return render(request, 'notes/notes_detail.html', {'note': note})
